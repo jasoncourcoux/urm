b10c5d510b2ae62e48ceee404205748db9215da5
Jason Courcoux <jxc1206@googlemail.com>
2016-09-22 14:34:31 +0100

    Starting to refactor the controls logic and remove some of the duplication

--- src/app/control.cljs
+++ src/app/control.cljs
@@ -9,66 +9,46 @@
                            (if (= 1 col) [x-coord y-coord])) row (iterate inc (:x t))))
                   (:coordinates t) (iterate inc (:y t)))))
 
+(defn can-move-to?
+  [state new-coordinates]
+  (every? true? (map (fn [[col row]]
+                       (cond (>= col (:grid-width state)) false
+                             (< col 0) false
+                             (>= row (count (:grid state))) false
+                             (empty? (get-in state [:grid row col])) true
+                             :else false)) new-coordinates)))
+
+(defn can-move?
+  [state axis transform]
+  (let [tetromino (:current-tetromino state)
+        new-coordinates (tetromino->coords (update tetromino axis transform))]
+    (can-move-to? state new-coordinates)))
+
 (defn can-move-down?
   [state]
-  (let [t (:current-tetromino state)
-        g (:grid state)
-        coords (tetromino->coords t)]
-    (every? true? (map (fn [[col row]]
-                         (cond (>= (inc row) (count g)) false
-                               (empty? (get-in g [(inc row) col])) true
-                               :else false)) coords))))
-
-(defn can-move-left?
-  [state]
-  (let [t (:current-tetromino state)
-        g (:grid state)
-        coords (tetromino->coords t)]
-    (every? true? (map (fn [[col row]]
-                         (cond (< (dec col) 0) false
-                               (empty? (get-in g [row (dec col)])) true
-                               :else false)) coords))))
-
-(defn can-move-right?
-  [state]
-  (let [t (:current-tetromino state)
-        g (:grid state)
-        coords (tetromino->coords t)]
-    (every? true? (map (fn [[col row]]
-                         (cond (>= (inc col) (:grid-width state)) false
-                               (empty? (get-in g [row (inc col)])) true
-                               :else false)) coords))))
+  (can-move? state :y inc))
 
 (defn can-rotate?
   [state]
-  (let [updated-coordinates (-> state :current-tetromino rotate-tetromino tetromino->coords)]
-        (every? true? (map (fn [[col row]]
-                             (cond (>= col (:grid-width state)) false
-                                   (<  col 0) false
-                                   (>= row (count (:grid state))) false
-                                   (empty? (get-in state [:grid row col])) true
-                                   :else false)) updated-coordinates))))
+  (can-move-to? state (-> state :current-tetromino rotate-tetromino tetromino->coords)))
 
-(defn move-left
-  [state]
-  (if (can-move-left? state) (update-in state [:current-tetromino :x] dec)
-                             state))
-
-(defn move-right
-  [state]
-  (if (can-move-right? state) (update-in state [:current-tetromino :x] inc)
-                              state))
+(defn move
+  [state axis transform]
+  (if (can-move? state axis transform)
+    (update-in state [:current-tetromino axis] transform)
+    state))
 
 (defn rotate
   [state]
-  (if (can-rotate? state) (update-in state [:current-tetromino] rotate-tetromino)
-                          state))
+  (if (can-rotate? state)
+    (update-in state [:current-tetromino] rotate-tetromino)
+    state))
 
 (defn merge-tetromino-into-grid [state]
   (let [type (:type (:current-tetromino state))
         coordinates (tetromino->coords (:current-tetromino state))]
-  (reduce (fn [s [col row]]
-            (assoc-in s [:grid row col] [type])) state coordinates)))
+    (reduce (fn [s [col row]]
+              (assoc-in s [:grid row col] [type])) state coordinates)))
 
 (defn move-down
   [state]
@@ -95,8 +75,8 @@
                            (do (.preventDefault e)
                                (if (= :running (:state @game-state))
                                  (cond
-                                   (= code LEFT) (swap! game-state move-left)
-                                   (= code RIGHT) (swap! game-state move-right)
+                                   (= code LEFT) (swap! game-state move :x dec)
+                                   (= code RIGHT) (swap! game-state move :x inc)
                                    (= code UP) (swap! game-state rotate)
                                    (= code DOWN) (swap! game-state move-down)
                                    (= code SPACE) (swap! game-state toggle-pause))
